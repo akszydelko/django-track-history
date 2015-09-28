129c12a5449f93527756d63422eb9c7220f45042
Arkadiusz Szydełko <arekxd@gmail.com>
2015-09-28 17:10:43 +0200

    Add methods to obtain model contributors.

--- track_history/manager.py
+++ track_history/manager.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from operator import attrgetter
 
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Manager
@@ -7,7 +8,7 @@ from django.utils.encoding import force_text
 from django.utils.functional import SimpleLazyObject
 
 from .query import SnapshotQuerySet
-from .utils import has_int_pk
+from .utils import has_int_pk, ContributorList
 
 
 # Import model in a different way, because of circular imports
@@ -70,6 +71,38 @@ class TrackHistoryManager(Manager):
     # Fields
     def get_field_history(self, field_name):
         return self.get_queryset().filter(changes__has=field_name)
+
+    # Editors
+    def get_contributors(self, user_query_only=()):
+        if isinstance(user_query_only, (list, tuple)):
+            user_query_only = ['user__' + x for x in user_query_only]
+
+        # We can not distinct users and order by date, so we will distinct users in the db query and order by date
+        # by ourselves, which should be faster when model will have many history records.
+        qs = sorted(self.get_queryset().only('user', 'date_created', 'record_type', *user_query_only)
+                    .select_related('user').distinct('user_id').order_by('user_id', 'record_type'),
+                    key=attrgetter('date_created'))
+
+        editors = []
+        creator = None
+        for history_record in qs:
+            if history_record.record_type == TrackHistoryRecord.RECORD_TYPES.created:
+                creator = history_record.user
+            editors.append(history_record.user)
+
+        return ContributorList(creator=creator, editors=editors)
+
+    def get_creator(self, user_query_only=()):
+        if not self.instance:
+            return None
+
+        if isinstance(user_query_only, (list, tuple)):
+            user_query_only = ['user__' + x for x in user_query_only]
+
+        thr = self.get_queryset().only('user', *user_query_only).filter(
+            record_type=TrackHistoryRecord.RECORD_TYPES.created).select_related('user').order_by('date_created').first()
+
+        return thr.user if thr else None
 
 
 class CreateAndReadOnlyManager(Manager):
--- track_history/utils.py
+++ track_history/utils.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class ContributorList(list):
+    def __init__(self, creator, editors):
+        super(ContributorList, self).__init__(editors)
+        self.creator = creator
+
+
 def has_int_pk(model):
     """Tests whether the given model has an integer primary key."""
     pk = model._meta.pk
