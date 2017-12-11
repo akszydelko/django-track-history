8b71ccf3a3d2c8d3dbd10a7d4760873faeebdd3c
Arkadiusz Szydełko <arekxd@gmail.com>
2017-12-11 23:27:26 +0100

    Update licence, readme and setup script.

--- README.md
+++ README.md
@@ -1,8 +1,44 @@
-django-track-history
+Django Track History
 ====================
 
-Django app for tracking model changes
+A simple model change tracking for Django.
+
+PostgreSQL exclusive as it uses some special database functions for better performance.
 
 
-### TODO:
-* Handle bulk operations `bulk_create`, `.objects.filter(...).update(...)` (If it's even possible)
+### Installation
+
+```bash
+pip install django-track-history
+```
+
+Then add `track_history` to `INSTALLED_APPS`.
+
+And add `track_history.middleware.TrackHistoryMiddleware` to the `MIDDLEWARE` list. 
+
+Decorate each model you want to track changes with `@track_changes`
+
+```python
+@track_changes
+class Author(models.Model):
+    ...
+```
+
+
+### Limitations
+
+Django Track History does not track changes for bulk operations like `bulk_create` or `.objects.filter(...).update(...)`. It is related to how Django handles that kind of queries.
+
+
+### Feature plans:
+* Better deferred model tests and tests for more complicated model structures
+* Django 2.0 support
+* Admin interface
+* EPIC: Handle bulk operations `bulk_create`, `.objects.filter(...).update(...)` (If it's even possible)
+
+
+### Legal
+
+Copyright (c) 2017, Arkadiusz Szydełko All rights reserved.
+
+Licensed under BSD 3-Clause License
--- setup.py
+++ setup.py
@@ -1,33 +1,45 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
+from track_history import __version__
 
-root = path.abspath(path.dirname(__file__))
-with open(path.join(root, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+long_description = """
+    A simple model change tracking for Django.
+    
+    More on: https://github.com/akszydelko/django-track-history
+    
+    Copyright (c) 2017, Arkadiusz Szydełko All rights reserved.
+
+    Licensed under BSD 3-Clause License
+    """
 
 setup(
     name='django-track-history',
-    version='0.0.1',
-    description='Easy change tracking for Django',
+    version=__version__,
+    description='Easy model change tracking for Django',
     long_description=long_description,
     url='https://github.com/akszydelko/django-track-history',
     author='Arkadiusz Szydelko',
     author_email='akszydelko@gmail.com',
-    license='MIT',
+    license='BSD',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: BSD License',
         'Environment :: Web Environment',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Framework :: Django',
-        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.10',
     ],
-    keywords='django history',
-    packages=find_packages(),
-    install_requires=['django>=1.8', 'django-pgjsonb>=0.0.16'],
+    keywords='django track history',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'django>=1.10',
+        'django-pgjsonb>=0.0.30'
+    ],
 )
